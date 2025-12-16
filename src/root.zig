4886c8396427d3a24f62ab124349f891f6e5029f
Shane Poppleton <shane@bjja.com.au>
2025-12-16 21:08:08 +1000

    Export SpriteLayout

--- src/root.zig
+++ src/root.zig
@@ -4,16 +4,20 @@ pub const Config = @import("config.zig").Config;
 pub const Context = @import("context.zig").Context;
 pub const GameVTable = @import("run.zig").GameVTable;
 pub const Gfx = @import("gfx.zig").Gfx;
+
 pub const types = struct {
     pub const Vec2 = rl.Vector2;
     pub const Color = rl.Color;
     pub const Rect = rl.Rectangle;
     pub const TraceLogLevel = rl.TraceLogLevel;
 };
+
 pub const graphics = struct {
     const sm = @import("graphics/sprite_manager.zig");
     pub const SpriteLayoutBuilder = sm.SpriteLayoutBuilder;
     pub const RotationSet = sm.RotationSet;
     pub const RotationFrame = sm.RotationFrame;
+    pub const SpriteLayout = sm.SpriteLayout;
 };
+
 pub const run = @import("run.zig").run;
